b653a556088b5c6e2bf5998e97c14dbdd75eaef6
Igor <aligol6573@gmail.com>
2026-09-18 14:04:57 +0300

    base

---
+++ sql_baSE.py
@@ -0,0 +1,36 @@
+import sqlite3
+
+conn = sqlite3.connect('test.db')
+cursor = conn.cursor()
+
+
+"""
+DDL - Создание таблиц
+DML - Заполнение, корректировка значений.....
+SELECT - получение данных
+"""
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS car
+(id INTEGER PRIMARY KEY NOT NULL,
+name TEXT)""")
+# cursor.execute("INSERT INTO car (name) VALUES (?)", ('Москвич',))
+# conn.commit()
+result = cursor.execute("SELECT * FROM car")
+# print(result.fetchall())
+for row, n in result:
+    print(f'Id: {row}, Name: {n}')
+"""
+SELECT * FROM car;
+SELECT name, year FROM car;
+SELECT name, year FROM car WHERE year > 2000;
+ALTER TABLE car ADD COLUMN type; 
+ALTER TABLE car RENAME COLUMN type TO energy; 
+INSERT INTO car (name, energy) VALUES('Toyota', 'diesel');
+SELECT name, year FROM car WHERE year BETWEEN 2004 AND 2026 ;
+
+SELECT name, year FROM car WHERE name in ('Mercedes', 'LADA');
+SELECT name, year FROM car WHERE year LIKE '202_' ;
+SELECT name, year FROM car WHERE year LIKE '20%'  AND name = 'Toyota';
+SELECT DISTINCT name FROM car ;
+SELECT name, year FROM car ORDER BY name DESC, year DESC;
+"""
