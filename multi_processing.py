4d7b09de68f8483b95d2c479f56f053c4f606e7c
Igor <aligol6573@gmail.com>
2026-09-19 12:45:06 +0300

    multi_processing

---
+++ multi_processing.py
@@ -0,0 +1,36 @@
+import multiprocessing as mp
+import time
+import random
+from math import sqrt
+
+
+def producer(queue, n_items):
+    for i in range(n_items):
+        item = sqrt(random.randint(1, 100)) ** 2
+        queue.put(item)
+        print(f'Производитель сделал {item}')
+        time.sleep(random.uniform(0.1, .5))
+    queue.put(None)
+    print('Производитель работу завершил!')
+
+
+def consumer(queue):
+    while True:
+        item = queue.get()
+        if item is None:
+            break
+        result = sqrt(item ** 2) ** 3
+        print(f'Получен {item} => {result}')
+        time.sleep(random.uniform(0.2, .6))
+    print('Пользователь работу завершил!')
+
+
+if __name__ == '__main__':
+    queue = mp.Queue()
+    p_producer = mp.Process(target=producer, args=(queue, 10))
+    p_consumer = mp.Process(target=consumer, args=(queue,))
+    p_producer.start()
+    p_consumer.start()
+    p_producer.join()
+    p_consumer.join()
+    print('Главный процесс завершен!')
